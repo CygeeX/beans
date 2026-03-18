25a1e2b88275d1ff0c13270e8988b7787954f9fa
gg <16470583+cyyard@user.noreply.gitee.com>
2026-03-18 17:28:06 +0800

    fix:小bug 路径错误

--- Frontend/src/api/user.js
+++ Frontend/src/api/user.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export function registerApi(data) {
-  return axios.post('http://localhost:8000/register', data)
+  return axios.post('http://localhost:8000/api/register', data)
 }
 
 export function loginApi(data) {
-  return axios.post('http://localhost:8000/login', data)
+  return axios.post('http://localhost:8000/api/login', data)
 }
